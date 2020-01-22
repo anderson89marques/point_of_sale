dae4b17ca5f9378125da1eb7a30dd89f877c9c72
anderson89marques <andersonoanjo18@gmail.com>
2020-01-22 03:52:13 -0300

    Add some tests to frontend

--- backend/point_of_sale/urls.py
+++ backend/point_of_sale/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
+from django.conf.urls import url, static
+from django.views.generic import TemplateView
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 from point_of_sale.api import urls
+from point_of_sale import settings
 
 
 schema_view = get_schema_view(
@@ -39,7 +41,5 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('api/', include(urls)),
     path('admin/', admin.site.urls),
-    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+    path('swagger/', TemplateView.as_view(template_name='index.html'))
+] + static.static(settings.STATIC_URL)
--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,51 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ShallowRenderer from 'react-test-renderer/shallow';
 import App from './App';
+import OrderItemInfo from './components/OrderItemInfo';
+import OrderItemForm from './components/OrderItemForm';
+import ListOrderItens from './components/ListOrderItens';
+import Header from './components/Header';
+import SaleData from './components/SaleData';
+import OrderModal from './components/OrderModal';
+import Pos from  './pages/pos';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+test('App component returns a div', () => {
+  const renderer = new ShallowRenderer();
+  renderer.render(<App />);
+  const result = renderer.getRenderOutput();
+  expect(result.type).toBe('div');
+});
+
+test('App component returns a correct children', () => {
+  const renderer = new ShallowRenderer();
+  renderer.render(<App />);
+  const result = renderer.getRenderOutput();
+  expect(result.props.children).toEqual([
+    <Header />,
+    <Pos />]);
+});
+
+test('Pos component returns a container class', () => {
+  const renderer = new ShallowRenderer();
+  renderer.render(<Pos />);
+  const result = renderer.getRenderOutput();
+  expect(result.type.displayName).toEqual('Container');
+});
+
+test('OrderItemInfo component returns a section', () => {
+  const renderer = new ShallowRenderer();
+  renderer.render(<OrderItemInfo />);
+  const result = renderer.getRenderOutput();
+  expect(result.type).toBe('section');
+  });
+
+
+test('OrderItemInfo component returns correct children', () => {
+  const renderer = new ShallowRenderer();
+  renderer.render(<OrderItemInfo />);
+  const result = renderer.getRenderOutput();
+  expect(result.props.children).toEqual([
+    <OrderItemForm />,
+    <ListOrderItens />]);
 });
